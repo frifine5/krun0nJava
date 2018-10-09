a55e601dd473cbfe50eee53a6936470d80453a17
wcy <wcy@gitlab.com>
2018-10-09 15:58:36 +0800

    上传测试。文件流部分Base64的问题

--- springmvc/pe-service/src/main/java/com/http/webservice/ctl/ESealPreController.java
+++ springmvc/pe-service/src/main/java/com/http/webservice/ctl/ESealPreController.java
@@ -1,11 +1,18 @@
 package com.http.webservice.ctl;
 
 
+import com.cer.SM2CaCert;
+import com.common.utils.ParamsUtil;
 import com.pe.entity.ApySealObj;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.pe.entity.SealItem;
+import net.sf.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Calendar;
+import java.util.Date;
 
 @RestController
 public class ESealPreController {
@@ -25,10 +32,45 @@ public class ESealPreController {
         return apySealObj;
     }
 
+    @RequestMapping(value = "/cert/apy2Rtca",  method = {RequestMethod.POST})
+    @ResponseBody
+    public Object exportSealEncData(@RequestParam("file") MultipartFile file, @RequestParam("sealItem") String sealItem){
+        byte[] byteContent = null;
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        try {
+            byteContent = file.getBytes();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(null == byteContent ){
+            return "未上传";
+        }
+        if(null != sealItem){
+            System.out.println(JSONObject.fromObject(sealItem));
+        }
+//        String fileStr = new String(byteContent, "UTF-8");
+//        System.out.println(fileStr);
+        try {
+//            Date[] valids = SM2CaCert.getSM2ValidTime(Base64.getDecoder().decode(fileStr.trim()));
+            Date[] valids = SM2CaCert.getSM2ValidTime(byteContent);
+            Calendar cld  = Calendar.getInstance();
+            cld.setTime(valids[0]);
+            int stYear = cld.get(Calendar.YEAR);
+            cld.setTime(valids[1]);
+            int age = cld.get(Calendar.YEAR) - stYear;
+            System.out.println(String.format("有效期%d年", age) );
+            return String.format("有效期从%s到%s", ParamsUtil.formatTime19(valids[0]),  ParamsUtil.formatTime19(valids[1]));
+        }catch (Exception e){
+            return e.getMessage();
+        }
+    }
 
 
 
 
 
 
-}
+
+
+    }
