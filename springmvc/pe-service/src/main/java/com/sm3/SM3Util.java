8c78c86ac79eb7b4c2121e369784956d99765e77
wcy <wcy@gitlab.com>
2019-01-04 09:12:13 +0800

    增加GM/T 0009中的sm3预处理摘要（带公钥摘要）

--- springmvc/pe-service/src/main/java/com/sm3/SM3Util.java
+++ springmvc/pe-service/src/main/java/com/sm3/SM3Util.java
@@ -17,4 +17,28 @@ public class SM3Util {
         sm3.doFinal(md, 0);
         return md;
     }
+
+    public static byte[] sm3Digest(byte[] in, byte[] pk){
+//        byte[] entl = {0x0,(byte)0x80};// 这个表示128bit的两位byte[]
+//        String hentl = Util.byteToHex(entl);
+        String hentl = "0080";
+        String hid = "31323334353637383132333435363738";
+        String ha = "FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC";
+        String hb = "28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93";
+        String hgx = "32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7";
+        String hgy = "BC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0";
+        String hpk = Util.byteToHex(pk);
+
+        String pre1 = hentl+hid+ha+hb+hgx+hgy+hpk.substring(2);
+        System.out.println(pre1);
+        byte[] z = sm3Digest(Util.hexToByte(pre1));
+
+        byte[] zm = Util.hexToByte(Util.byteToHex(z)+Util.byteToHex(in));
+        byte[] md = sm3Digest(zm);
+
+        return md;
+    }
+
+
+
 }
