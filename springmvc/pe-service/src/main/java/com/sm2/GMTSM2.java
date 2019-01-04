4ddda97f2f8364b7e70fb071ca8aa35a4c5844c8
wcy <wcy@gitlab.com>
2019-01-04 16:35:03 +0800

    在GMTSM2中增加带公钥的摘要

--- springmvc/pe-service/src/main/java/com/sm2/GMTSM2.java
+++ springmvc/pe-service/src/main/java/com/sm2/GMTSM2.java
@@ -177,14 +177,23 @@ public class GMTSM2 {
      * 带公钥的摘要 sm3(pk||m)
      */
     public byte[] sm3Degest(byte[] data, byte[] puk) {
-        SM3Digest sm3 = new SM3Digest();
-        byte[] hashData = new byte[32];
-        byte[] inData = new byte[data.length + puk.length];
-        System.arraycopy(puk, 0, inData, 0, puk.length);
-        System.arraycopy(data, 0, inData, puk.length, data.length);
-        sm3.update(inData, 0, data.length);
-        sm3.doFinal(hashData, 0);
-        return hashData;
+        String hentl = "0080";
+        String hid = "31323334353637383132333435363738";
+        String ha = "FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC";
+        String hb = "28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93";
+        String hgx = "32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7";
+        String hgy = "BC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0";
+        String hpk = StrUtil.byteToHex(puk);
+
+        // 1
+        String pre1 = hentl+hid+ha+hb+hgx+hgy+hpk.substring(2);
+        System.out.println(pre1);
+        byte[] z = sm3Degest(StrUtil.hexToByte(pre1));
+
+        // 2
+        byte[] zm = StrUtil.hexToByte(StrUtil.byteToHex(z)+StrUtil.byteToHex(data));
+        byte[] md = sm3Degest(zm);
+        return md;
     }
 
     /**
