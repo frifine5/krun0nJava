f0e65b467fdc32eb0cb41547e42afe2b6ed484be
wcy <wcy@gitlab.com>
2019-01-24 11:29:52 +0800

    添加https支持的方法in springboot：但是这个只能http和https二选一，无法并存。并存方案可参nginx+springboot

--- springmvc/pe-service/src/main/java/com/PEServiceStart.java
+++ springmvc/pe-service/src/main/java/com/PEServiceStart.java
@@ -1,9 +1,16 @@
 package com;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -18,17 +25,51 @@ import org.springframework.context.annotation.Bean;
 @EnableHystrix
 @EnableHystrixDashboard
 @MapperScan(basePackages = {"com..pe.*"})
-public class PEServiceStart
-{
+public class PEServiceStart {
     public static void main(String[] args) {
-            SpringApplication.run(PEServiceStart.class, args);
+        SpringApplication.run(PEServiceStart.class, args);
     }
 
     @Bean
-    public MapperScannerConfigurer apperScannerConfigurer()
-    {
+    public MapperScannerConfigurer apperScannerConfigurer() {
         MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
         mapperScannerConfigurer.setBasePackage("com.**.dao");
         return mapperScannerConfigurer;
     }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer(){
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
+
+            @Override
+            protected void postProcessContext(Context context) {
+
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
+                securityConstraint.addCollection(collection);
+
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Value("$server.port:8080")
+    int defPort;
+
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(defPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
 }
