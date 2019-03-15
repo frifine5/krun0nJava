9bb870c5e7d793ba3e49f7ac2fb0ada5336eb887
wcy <wcy@gitlab.com>
2019-03-15 16:14:30 +0800

    增加写文件通用方法1

--- springmvc/pe-service/src/main/java/com/common/utils/FileUtil.java
+++ springmvc/pe-service/src/main/java/com/common/utils/FileUtil.java
@@ -113,4 +113,13 @@ public class FileUtil {
     }
 
 
+    public static byte[] writeInFiles(String path, String txt) throws IOException {
+        OutputStream out = new FileOutputStream(new File(path));
+        out.write(txt.getBytes());
+        out.flush();
+        out.close();
+        return txt.getBytes();
+    }
+
+
 }
