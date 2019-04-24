5ce3d1fe5e76e879241175e42e6442a08da7aae9
wcy <wcy@gitlab.com>
2019-04-24 11:10:46 +0800

    change algorithm

--- springmvc/pe-service/src/test/java/com/gomain/makeseal/demo/TeGetSM3message.java
+++ springmvc/pe-service/src/test/java/com/gomain/makeseal/demo/TeGetSM3message.java
@@ -2,6 +2,7 @@ package com.gomain.makeseal.demo;
 
 import com.common.utils.ParamsUtil;
 import com.sm3.SM3Util;
+import org.junit.Test;
 
 import java.util.Base64;
 import java.util.Date;
@@ -10,8 +11,7 @@ import java.util.UUID;
 public class TeGetSM3message {
 
 
-
-    private static void sm3dg(){
+    private static void sm3dg() {
         String uuid = UUID.randomUUID().toString().replaceAll("-", "");
         System.out.println();
         String split = "_";
@@ -26,7 +26,28 @@ public class TeGetSM3message {
     }
 
 
+    @Test
+    public void test1() {
 
+        int a, b;
+
+        a = 5;
+        b = 9;
+        System.out.printf("a=%d, b=%d", a, b);
+        System.out.println();
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.printf("a=%s, b=%s", a, b);
+        System.out.println();
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.printf("a=%s, b=%s", a, b);
+        System.out.println();
+
+    }
 
 
 }
