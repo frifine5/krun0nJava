d979717c4afb36a48976dfb3d69b9387cc4baee2
wcy <wcy@gitlab.com>
2019-08-01 17:21:41 +0800

    reload method with judge length

--- springmvc/pe-service/src/main/java/com/sm2/SM2EncUtil.java
+++ springmvc/pe-service/src/main/java/com/sm2/SM2EncUtil.java
@@ -70,7 +70,47 @@ public class SM2EncUtil {
          */
         byte[] c1Bytes = StrUtil.hexToByte(data.substring(0, 130));
         byte[] c3 = StrUtil.hexToByte(data.substring(130, 130 + 64));
-        byte[] c2 = StrUtil.hexToByte(data.substring(130 + 64));
+        byte[] c2= StrUtil.hexToByte(data.substring(130 + 64));
+
+        SM2 sm2 = SM2.getInstance();
+        BigInteger userD = new BigInteger(1, privateKey);
+
+        //通过C1实体字节来生成ECPoint
+        ECPoint c1 = sm2.ecc_curve.decodePoint(c1Bytes);
+        Cipher cipher = new Cipher();
+        cipher.Init_dec(userD, c1);
+        cipher.Decrypt(c2);
+        cipher.Dofinal(c3);
+
+        //返回解密结果
+        return c2;
+    }
+
+
+    //数据解密
+    public static byte[] decrypt(byte[] privateKey, byte[] encryptedData, boolean hasLen) throws IOException {
+        if (privateKey == null || privateKey.length == 0) {
+            return null;
+        }
+
+        if (encryptedData == null || encryptedData.length == 0) {
+            return null;
+        }
+        //加密字节数组转换为十六进制的字符串 长度变为encryptedData.length * 2
+        String data = StrUtil.byteToHex(encryptedData);
+        /***分解加密字串
+         * （C1 = C1标志位2位 + C1实体部分128位 = 130）
+         * （C3 = C3实体部分64位  = 64）
+         * （C2 = encryptedData.length * 2 - C1长度  - C2长度）
+         */
+        byte[] c1Bytes = StrUtil.hexToByte(data.substring(0, 130));
+        byte[] c3 = StrUtil.hexToByte(data.substring(130, 130 + 64));
+        byte[] c2;
+        if(hasLen){
+            c2 = StrUtil.hexToByte(data.substring(130 + 64 + 8));
+        }else{
+            c2 = StrUtil.hexToByte(data.substring(130 + 64));
+        }
 
         SM2 sm2 = SM2.getInstance();
         BigInteger userD = new BigInteger(1, privateKey);
@@ -119,7 +159,7 @@ public class SM2EncUtil {
         String cipherText = SM2EncUtil.encrypt(StrUtil.hexToByte(pubk), sourceData);
         System.out.println(cipherText);
         System.out.println("解密: ");
-        String decText = new String(SM2EncUtil.decrypt(StrUtil.hexToByte(prik), StrUtil.hexToByte(cipherText)));
+        String decText = new String(SM2EncUtil.decrypt(StrUtil.hexToByte(prik), StrUtil.hexToByte(cipherText), false));
         System.out.println(decText);
 
     }
