f10cf85e1cffe0c4e54d53a45c0d9c4ffebb4f15
wangchengyu <wangchengyu@baiwang.com>
2019-12-23 18:08:59 +0800

    add new project

---
+++ springmvc/pos-self-service/src/main/java/com/AppServiceStarter.java
@@ -0,0 +1,20 @@
+package com;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(scanBasePackages = {"com.http" })
+@ComponentScan("com.*")
+@MapperScan(basePackages = {"com.pos.*.dao.*"})
+public class AppServiceStarter {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServiceStarter.class, args);
+        System.out.println("starting...");
+    }
+
+}
