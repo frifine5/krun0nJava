7ab1845600fddd257b26fc8bea4156d36ec0363b
wangchengyu <wangchengyu@baiwang.com>
2021-02-01 15:38:38 +0800

    单例模式：加注释

--- springmvc/pe-service/src/main/java/com/cyk/dsp/t/ch1/singleton/HungrySingleton.java
+++ springmvc/pe-service/src/main/java/com/cyk/dsp/t/ch1/singleton/HungrySingleton.java
@@ -1,5 +1,9 @@
 package com.cyk.dsp.t.ch1.singleton;
 
+
+/**
+ * 饿汉模式：无论是否使用实例，在第一次被引用构建时，即将所需的内存分配给它
+ */
 public class HungrySingleton {
 
 
--- springmvc/pe-service/src/main/java/com/cyk/dsp/t/ch1/singleton/LazySingleton.java
+++ springmvc/pe-service/src/main/java/com/cyk/dsp/t/ch1/singleton/LazySingleton.java
@@ -1,5 +1,9 @@
 package com.cyk.dsp.t.ch1.singleton;
 
+
+/**
+ * 懒汉模式：初始使用时，不立即分配空间，而在第一次调用时。但调用时需要加锁来保持唯一性；并用volatile来防止指令重排。
+ */
 public class LazySingleton {
 
 
@@ -12,6 +16,22 @@ public class LazySingleton {
         return singleton;
     }
 
+    /**
+     * 双锁机制下：无volatile会因为指令重排的可能，而拿到一个已申明，但未初始化的对象。
+     * @param args
+     */
+/*
+    public static synchronized LazySingleton getInstance(){
+        if (null==singleton){
+            synchronized (LazySingleton.class){
+                if(null == singleton){
+                    singleton = new LazySingleton();
+                }
+            }
+        }
+        return singleton;
+    }
+*/
 
     public static void main(String[] args) {
 
